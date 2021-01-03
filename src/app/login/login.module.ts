56530963f7c3c988a570a940080ef24644d87c97
NataliaRajska <natalia.rajskaa@gmail.com>
2021-01-03 19:47:45 +0100

    Add route to login component

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './login.component';
-import {RegistrationRoutingModule} from "../registration/registration-routing.module";
-import {LoginRoutingModule} from "./login-routing.module";
+import { LoginRoutingModule } from './login-routing.module';
 
 
 @NgModule({
