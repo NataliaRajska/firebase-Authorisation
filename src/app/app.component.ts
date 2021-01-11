591598044ed5107a65295ee63cbb9c3f2e518238
NataliaRajska <natalia.rajskaa@gmail.com>
2021-01-11 16:28:00 +0100

    Change style in login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,11 @@
 import {Component, OnDestroy} from '@angular/core';
-import {FirebaseService} from './services/firebase.service';
-import {OnInit} from '@angular/core';
-import {take} from 'rxjs/operators';
-import {GoogleLoginProvider, SocialAuthService, SocialUser} from 'angularx-social-login';
-import {Subscription} from 'rxjs';
-import {AuthMethodEnum, LoggedUserModel} from './models/loggedUser.model';
-import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-
-  constructor(
-    private router: Router) {
-  }
-
-  public goToRoute(): void {
-    this.router.navigate(['registration']);
-  }
-}
+export class AppComponent { }
 
 
 
